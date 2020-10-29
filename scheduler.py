32f5e0ea8ca5a28218137d197f8d4b9be784747c
PunkmeisterDuy <punkmeister@protonmail.com>
2020-10-29 09:46:31 -0700

    base

---
+++ scheduler.py
@@ -0,0 +1,42 @@
+import random
+
+class Task:
+    def __init__(self, name = "", importance = -1):
+        self.name = name
+        self.importance = importance
+
+class Schedule:
+    def __init__(self):
+        self.tasks=[]
+    
+    def add_task(self, Task):
+        self.tasks.append(Task)
+
+    def ask_task(self):
+        ask = True
+        while(ask):
+            taskName = input("Enter name of task (blank to exit): ")
+            if (taskName == ""):
+                ask = False
+            else:
+                importance = int(input("Enter importance (1-10): "))
+                self.add_task(Task(taskName, importance))
+    
+    def volatile_randomizer(self):
+        volatileSchedule = Schedule()
+        for i in range(len(self.tasks)):
+            volatileSchedule.tasks.append(self.tasks[i])
+        for i in range(len(volatileSchedule.tasks)):
+            volatileSchedule.tasks[i].importance = volatileSchedule.tasks[i].importance + (random.randint(0,100) * .01)
+        return volatileSchedule
+
+schedule = Schedule()
+
+
+schedule.ask_task()
+print(schedule.tasks[0].name)
+print(schedule.tasks[0].importance)
+dailySchedule = schedule.volatile_randomizer()
+print(dailySchedule.tasks[0].importance)
+
+#FIX THE ""
